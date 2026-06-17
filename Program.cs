60bc2456e2d275f7b2af47127ebd0196e96d6f90
Aumulet-max <tanavat.bo.67@ubu.ac.th>
2026-06-17 10:21:03 +0700

    Learning Array

--- Program.cs
+++ Program.cs
@@ -12,7 +12,20 @@ namespace LearnCS
         {
             Console.WriteLine("Hello World!");
             int a = 5, b = 2;
+            // Addition
             Console.WriteLine($"a + b = {a + b}");
+            /*
+             * Multi line comment
+             */
+
+            // Array
+            string[] cars = new string[3];
+            cars[0] = "Ford";
+            cars[1] = "BMW";
+            cars[2] = "Toyota";
+            Console.WriteLine($"The first car is {cars[0]}");
+            Console.WriteLine($"The second car is {cars[1]}");
+            Console.WriteLine($"The third car is {cars[2]}");
 
         }
     }
